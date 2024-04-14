95953985ed2c4d8b2f1b5b242dacd6e548935565
Sa18-f <sisazid182713@gmail.com>
2024-04-14 16:06:25 +0600

    setup finised

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh2gB0er-mq5XuUYIYoGJPtUpScv39mtM",
+  authDomain: "real-estate-auth-b1fd5.firebaseapp.com",
+  projectId: "real-estate-auth-b1fd5",
+  storageBucket: "real-estate-auth-b1fd5.appspot.com",
+  messagingSenderId: "652021206778",
+  appId: "1:652021206778:web:56cd7113f43b5f1c8c0343"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
